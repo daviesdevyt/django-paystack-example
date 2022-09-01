c943b5466dbbc192b9eab36ad6c669c884c5d9c8
unknown <davidonzan@gmail.com>
2022-09-01 18:12:11 +0100

    env vars, frontend paystack integration

---
+++ base/static/index.js
@@ -0,0 +1,34 @@
+const paymentForm = document.querySelector("#paymentForm");
+paymentForm.addEventListener("submit", payWithPaystack, false);
+
+fetch("/public_key")
+.then(res => res.json())
+.then(data => public_key = data)
+
+function payWithPaystack(e) {
+  e.preventDefault();
+  let handler = PaystackPop.setup({
+    key: public_key, // Replace with your public key
+    email: document.getElementById("email").value,
+    amount: document.getElementById("amount").value * 100,
+    ref: ''+Math.floor((Math.random() * 1000000000) + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
+    // label: "Optional string that replaces customer email"
+    onClose: function(){
+      alert('Window closed.');
+    },
+    callback: function(response){
+      let message = 'Payment complete! Reference: ' + response.reference;
+      alert(message);
+      $.ajax({
+        url: '/verify_transaction?reference='+ response.reference,
+        method: 'get',
+        success: function (response) {
+            console.log(response.data.status)
+          // the transaction status is in response.data.status
+        }
+      });
+    }
+  });
+
+  handler.openIframe();
+}
--- base/views.py
+++ base/views.py
@@ -1,5 +1,11 @@
+import imp
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.conf import settings
 
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+
+def get_public_key(request):
+    return JsonResponse(settings.PAYSTACK_PUBLIC_KEY, safe=False)
